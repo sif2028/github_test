cb7d9423ce3daa11120f437c596122091f3d8c13
sif2028 <sif2028@nyu.edu>
2025-11-04 18:07:49 -0500

    added led

--- github_test.ino
+++ github_test.ino
@@ -1,9 +1,10 @@
-void setup() {
-  // put your setup code here, to run once:
+int ledPin = 13;
 
+void setup() {
+  pinMode(ledPin, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+
 
 }
