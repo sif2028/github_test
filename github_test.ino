98401cfbfc1ade9856ca8005518147989140a041
sif2028 <sif2028@nyu.edu>
2025-11-04 18:18:14 -0500

    Update github_test.ino

--- github_test.ino
+++ github_test.ino
@@ -2,6 +2,7 @@ int ledPin = 29;
 int ledPin2 = 30;
 
 void setup() {
+  Serial.begin(9600);
   pinMode(ledPin, OUTPUT);
   pinMode(ledPin2, OUTPUT);
 }
@@ -9,8 +10,12 @@ void setup() {
 void loop() {
   digitalWrite(ledPin, HIGH);
   digitalWrite(ledPin2, HIGH);
+  Serial.println(digitalRead(ledPin));
+  Serial.println(digitalRead(ledPin2));
   delay(500);
   digitalWrite(ledPin, LOW);
   digitalWrite(ledPin2, LOW);
+  Serial.println(digitalRead(ledPin));
+  Serial.println(digitalRead(ledPin2));
   delay(500);
 }
